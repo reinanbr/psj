535a135c2ec36c428d5e51faf50089d3baca4097
reinanbr <perseu912@gmail.com>
2023-10-17 07:10:51 -0300

    added comment

--- simulations/electromagnetism/fieldElectric/parallelCapacitor/static/js/html_works.js
+++ simulations/electromagnetism/fieldElectric/parallelCapacitor/static/js/html_works.js
@@ -128,3 +128,58 @@ src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-
     <!-- Copyright -->
 </footer>
 `)
+
+
+
+function alertBt(title, text, idDivPainel, mode) {
+  $(`#${idDivPainel}`).append(` <div class="alert alert-${mode} alert-dismissible fade show" role="alert">
+  <strong>${title}</strong> ${text}
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>`)
+}
+
+
+
+function sendFeedBack(){
+    let feed = $('#inputComment').val()
+    console.log(feed)
+    let userAgent = navigator.userAgent;
+    let urlName = window.location.href
+    fetch(`https://reysofts.com.br/apis/cardapio/save_comment.php?userDevice=${userAgent}&urlName=${urlName}&comment=${feed}`)
+        .then((res) => {
+            console.log(res)
+            if (!res.ok) {
+                
+                throw new Error('ERROR IN REQUEST: ' + res.status);
+              }
+            return res.json()
+        }).then((statusComment) => {
+            if (statusComment.result) {
+
+                alertBt(`Muito obrigado!`,
+                    ` Seu comentário foi enviado e salvo com sucesso!`,
+                    'painelAlert',
+                    'success')
+
+                $('#inputComment').val('')
+            }
+            else {
+                alertBt(`Oops!`,
+                    ` Tivemos algum problema com salvamento de seu comentario. Tente novamente mais tarde.`,
+                    'painelAlert',
+                    'Alert')
+
+            }
+        })
+        .catch((e) => {
+            alertBt('Oops!',
+            `Tivemos algum problema com o nosso servidor. Tente novamente mais tarde. ERROR: ${e}`,
+            'painelAlert',
+            'danger')
+            console.log(e)
+        })
+}
+
+$('#btnSendFeed').click(sendFeedBack)
